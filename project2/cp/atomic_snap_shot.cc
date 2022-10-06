19cff0a1439156123984efe2e7a980f5f8d69340
2019082851 <2019082851@hanyang.ac.kr>
2022-10-06 12:17:43 +0900

    project02_seperate_code_header_and_src

--- project2/cp/atomic_snap_shot.cc
+++ project2/cp/atomic_snap_shot.cc
@@ -1,100 +1,85 @@
-template<typename T>
-class StampedSnap{
-	public:
-	long stamp;
-	T value;
-	T *snap;
-	StampedSnap(T val){
-		stamp =0;
-		value = val;
-		snap = 0;
-	}
-	StampedSnap(long label,T val,T* snp){
-		stamp = label;
-		value = val;
-		snap = snp;
-	}
-	StampedSnap(){
-		stamp = 0;
-		snap = 0;
-	}
+#include "atomic_snap_shot.h"
 
+
+StampedSnap::StampedSnap(T val){
+	stamp =0;
+	value = val;
+	snap = 0;
+}
+
+tampedSnap::StampedSnap(long label,T val,T* snp){
+	stamp = label;
+	value = val;
+	snap = snp;
+}
+StampedSnap::StampedSnap(){
+	stamp = 0;
+	snap = 0;
+}
+
+WFSnapshot::StampedSnap<T>** collect(){
+	StampedSnap<T>** copy =(StampedSnap<T>**) new StampedSnap<T>*[len];
+	for (int j = 0; j < len; j++) {
+		copy[j] = new StampedSnap<T>(a_table[j]->stamp,a_table[j]->value,a_table[j]->snap);  
+	}
+	return copy;
+}	
 	
-};
-
-template<typename T>
-class WFSnapshot {
-	private:
-	StampedSnap<T>** a_table;
-	int len;
-	StampedSnap<T>** collect(){
-		StampedSnap<T>** copy =(StampedSnap<T>**) new StampedSnap<T>*[len];
-		for (int j = 0; j < len; j++) {
-			copy[j] = new StampedSnap<T>(a_table[j]->stamp,a_table[j]->value,a_table[j]->snap);  
-		}
-		return copy;
-	}	
-	
-
-	public:
-	WFSnapshot(int capacity, T init) {
-		a_table = (StampedSnap<T>**) new StampedSnap<T>*[capacity];
-		this->len = capacity;
-		for (int i = 0; i < capacity; i++) {
-			a_table[i] = new StampedSnap<T>(init); 
-		}
+WFSnapshot::WFSnapshot(int capacity, T init) {
+	a_table = (StampedSnap<T>**) new StampedSnap<T>*[capacity];
+	this->len = capacity;
+	for (int i = 0; i < capacity; i++) {
+		a_table[i] = new StampedSnap<T>(init); 
 	}
+}
 	
-	void update(T value,int thread_id){
-		//printf("u v:%d t:%d\n");
-		int id = thread_id;
-		T* snap = scan();
-		StampedSnap<T>* oldVal = a_table[id];
-		StampedSnap<T>* newVal = new StampedSnap<T>(oldVal->stamp+1,value,snap);
-		delete oldVal;
-		a_table[id] = newVal;
+void WFSnapshot::update(T value,int thread_id){
+	//printf("u v:%d t:%d\n");
+	int id = thread_id;
+	T* snap = scan();
+	StampedSnap<T>* oldVal = a_table[id];
+	StampedSnap<T>* newVal = new StampedSnap<T>(oldVal->stamp+1,value,snap);
+	delete oldVal;
+	a_table[id] = newVal;
+}
+
+T* WFSnapshot::scan(){
+	StampedSnap<T>** oldcopy;
+	StampedSnap<T>** newcopy;
+	bool *moved = new bool[len];
+
+	for(int j = 0 ; j  < len ; j ++){
+		moved[j] = false;
 	}
 
-	T* scan(){
-		StampedSnap<T>** oldcopy;
-		StampedSnap<T>** newcopy;
-		bool *moved = new bool[len];
+	oldcopy = collect();
 
-		for(int j = 0 ; j  < len ; j ++){
-			moved[j] = false;
-		}
+	COLLECT:
+	newcopy = collect();
+	for(int j = 0 ; j  < len ; j ++){
+		if(oldcopy[j]->stamp != newcopy[j]->stamp){
+			if(moved[j]) return oldcopy[j]->snap;
+			else {
+				moved[j] = true;
+				oldcopy = newcopy;
+					for (int j = 0; j < len; j++) {
+							delete oldcopy[j];
+					}
 
-		oldcopy = collect();
-
-		COLLECT:
-		newcopy = collect();
-		for(int j = 0 ; j  < len ; j ++){
-			if(oldcopy[j]->stamp != newcopy[j]->stamp){
-				if(moved[j]) return oldcopy[j]->snap;
-				else {
-					moved[j] = true;
-					oldcopy = newcopy;
-					    for (int j = 0; j < len; j++) {
-								delete oldcopy[j];
-					    }
-
-					goto COLLECT;
-				}
+				goto COLLECT;
 			}
-		}	
+		}
+	}	
     
-    for (int j = 0; j < len; j++) {
-			delete oldcopy[j];
-		}   
+	for (int j = 0; j < len; j++) {
+		delete oldcopy[j];
+	}   
 	  
-		oldcopy = newcopy;		
-		T* result = new T[len];
-		for(int j = 0 ; j < len ; j ++){
-			result[j] = newcopy[j]->value;
-		} 
-		delete oldcopy;
-		return result;
-	}
-
-};
-
+	oldcopy = newcopy;		
+	T* result = new T[len];
+	for(int j = 0 ; j < len ; j ++){
+		result[j] = newcopy[j]->value;
+	} 
+	delete oldcopy;
+	return result;
+}
--- project2/cp/atomic_snap_shot.h
+++ project2/cp/atomic_snap_shot.h
@@ -4,22 +4,9 @@ class StampedSnap{
 	long stamp;
 	T value;
 	T *snap;
-	StampedSnap(T val){
-		stamp =0;
-		value = val;
-		snap = 0;
-	}
-	StampedSnap(long label,T val,T* snp){
-		stamp = label;
-		value = val;
-		snap = snp;
-	}
-	StampedSnap(){
-		stamp = 0;
-		snap = 0;
-	}
-
-	
+	StampedSnap(T val);
+	StampedSnap(long label,T val,T* snp);
+	StampedSnap();
 };
 
 template<typename T>
@@ -27,74 +14,15 @@ class WFSnapshot {
 	private:
 	StampedSnap<T>** a_table;
 	int len;
-	StampedSnap<T>** collect(){
-		StampedSnap<T>** copy =(StampedSnap<T>**) new StampedSnap<T>*[len];
-		for (int j = 0; j < len; j++) {
-			copy[j] = new StampedSnap<T>(a_table[j]->stamp,a_table[j]->value,a_table[j]->snap);  
-		}
-		return copy;
-	}	
+	StampedSnap<T>** collect();	
 	
 
 	public:
-	WFSnapshot(int capacity, T init) {
-		a_table = (StampedSnap<T>**) new StampedSnap<T>*[capacity];
-		this->len = capacity;
-		for (int i = 0; i < capacity; i++) {
-			a_table[i] = new StampedSnap<T>(init); 
-		}
-	}
+	WFSnapshot(int capacity, T init);
 	
-	void update(T value,int thread_id){
-		//printf("u v:%d t:%d\n");
-		int id = thread_id;
-		T* snap = scan();
-		StampedSnap<T>* oldVal = a_table[id];
-		StampedSnap<T>* newVal = new StampedSnap<T>(oldVal->stamp+1,value,snap);
-		delete oldVal;
-		a_table[id] = newVal;
-	}
+	void update(T value,int thread_id);
 
-	T* scan(){
-		StampedSnap<T>** oldcopy;
-		StampedSnap<T>** newcopy;
-		bool *moved = new bool[len];
-
-		for(int j = 0 ; j  < len ; j ++){
-			moved[j] = false;
-		}
-
-		oldcopy = collect();
-
-		COLLECT:
-		newcopy = collect();
-		for(int j = 0 ; j  < len ; j ++){
-			if(oldcopy[j]->stamp != newcopy[j]->stamp){
-				if(moved[j]) return oldcopy[j]->snap;
-				else {
-					moved[j] = true;
-					oldcopy = newcopy;
-					    for (int j = 0; j < len; j++) {
-								delete oldcopy[j];
-					    }
-
-					goto COLLECT;
-				}
-			}
-		}	
-    
-    for (int j = 0; j < len; j++) {
-			delete oldcopy[j];
-		}   
-	  
-		oldcopy = newcopy;		
-		T* result = new T[len];
-		for(int j = 0 ; j < len ; j ++){
-			result[j] = newcopy[j]->value;
-		} 
-		delete oldcopy;
-		return result;
-	}
+	T* scan();
 
 };
 
