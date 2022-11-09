437380e6ecf9183e127795883f33395ddd467388
2019082851 <2019082851@hanyang.ac.kr>
2022-11-09 16:25:54 +0900

    project3_init

---
+++ project3/debug/BwtreeConfig.h
@@ -0,0 +1,5 @@
+// The configured options and settings for Bwtree project
+#define BwtreePlus_VERSION_MAJOR 
+#define BwtreePlus_VERSION_MINOR 
+/* #undef USE_BWTREE */
+/* #undef USE_GOOGLE_TEST */
