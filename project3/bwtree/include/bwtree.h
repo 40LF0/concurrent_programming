65987f9761c19238c25d7f43dd99df01460b49f5
2019082851 <2019082851@hanyang.ac.kr>
2022-11-20 00:40:38 +0900

    implement sol

--- project3/bwtree/include/bwtree.h
+++ project3/bwtree/include/bwtree.h
@@ -1918,7 +1918,7 @@ class BwTree : public BwTreeBase {
     NO_ASAN inline static ElasticNode *Get(int size,  // Number of elements
                                            NodeType p_type, int p_depth,
                                            int p_item_count,  // Usually equal to size
-                                           const KeyNodeIDPair &p_low_key, const KeyNodeIDPair &p_high_key, uint64_t time_stamp) {
+                                           const KeyNodeIDPair &p_low_key, const KeyNodeIDPair &p_high_key, uint64_t time_stamp = 0) {
       // Currently this is always true - if we want a larger array then
       // just remove this line
       NOISEPAGE_ASSERT(size == p_item_count, "Remove this if you want a larger array.");
