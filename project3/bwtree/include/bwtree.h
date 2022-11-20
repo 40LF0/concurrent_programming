60c705cd9cce7e72814ac46adf2edeaf262ee0f1
2019082851 <2019082851@hanyang.ac.kr>
2022-11-20 23:42:50 +0900

    rollback

--- project3/bwtree/include/bwtree.h
+++ project3/bwtree/include/bwtree.h
@@ -5659,13 +5659,15 @@ class BwTree : public BwTreeBase {
   NO_ASAN inline void ConsolidateLeafNode(NodeSnapshot *snapshot_p) {
     NOISEPAGE_ASSERT(snapshot_p->node_p->IsOnLeafDeltaChain(), "Leaf node must be on delta chain.");
 
+    /*
+    //2022-11-20
     NodeID current_node_id = snapshot_p->node_id;
     bool expected = false;
     bool ret_flag = leaf_consolidation_flag[current_node_id].compare_exchange_strong(expected, true);
     if(!ret_flag){
         return;
     }
-
+    */
 
     LeafNode *leaf_node_p = CollectAllValuesOnLeaf(snapshot_p);
 
@@ -5679,12 +5681,12 @@ class BwTree : public BwTreeBase {
       epoch_manager.AddGarbageNode(leaf_node_p);
     }
 
-
-
+    /*
+    //2022-11-20
     expected = true;
     ret_flag = leaf_consolidation_flag[current_node_id].compare_exchange_strong(expected, false);
     NOISEPAGE_ASSERT(ret_flag == true, "ret_flag should be always true");
-
+    */
   }
 
   /*
