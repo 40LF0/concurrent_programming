9c6292ca604325a343d63733e78c6c2ba6568274
2019082851 <2019082851@hanyang.ac.kr>
2022-11-26 14:30:23 +0900

    modify logic

--- project3/bwtree/include/bwtree.h
+++ project3/bwtree/include/bwtree.h
@@ -5709,7 +5709,8 @@ class BwTree : public BwTreeBase {
     // we have to modify InstallNodeToReplace logic
     //bool ret = InstallNodeToReplace(snapshot_p->node_id, leaf_node_p, snapshot_p->node_p);
     const BaseNode *expected_node = node_child;
-    bool ret = (static_cast<const DeltaNode *>(snapshot_p->node_p))->child_node_p.compare_exchange_strong(expected_node, leaf_node_p);
+    const BaseNode *changede_node = leaf_node_p;
+    bool ret = (static_cast<const DeltaNode *>(snapshot_p->node_p))->child_node_p.compare_exchange_strong(expected_node, changede_node);
 
 
     if (ret) {
